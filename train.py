44b365c217ef90d67e8d717394adb9f13683d82e
waiting-gy <904450727@qq.com>
2019-10-11 00:26:40 +0800

    Update train.py

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from coco_utils import get_coco, get_coco_kp
 from group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
 from engine import train_one_epoch, evaluate
 
-import utils
+import utilsdd
 import transforms as T
 
 
